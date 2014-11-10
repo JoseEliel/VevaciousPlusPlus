daa36d1f5e82eab10c777444e2627242fd839e34
Ben O'Leary <benjamin.oleary@gmail.com>
2014-11-10 18:14:45 +0100

    Forgot to add in files with new name in the last commit. Also prepared TunnelPath and derived classes for using Eigen::VectorXd objects as well as std::vector< double > objects.

---
+++ VevaciousPlusPlus/source/BounceActionEvaluation/BounceActionPathFinding/MinuitOnPathNormalInertialPotential.cpp
@@ -0,0 +1,24 @@
+/*
+ * MinuitOnPathNormalInertialPotential.cpp
+ *
+ *  Created on: Nov 10, 2014
+ *      Author: bol
+ */
+
+#include "BounceActionEvaluation/BounceActionPathFinding/MinuitOnPathNormalInertialPotential.hpp"
+
+namespace VevaciousPlusPlus
+{
+
+  MinuitOnPathNormalInertialPotential::MinuitOnPathNormalInertialPotential()
+  {
+    // TODO Auto-generated constructor stub
+
+  }
+
+  MinuitOnPathNormalInertialPotential::~MinuitOnPathNormalInertialPotential()
+  {
+    // TODO Auto-generated destructor stub
+  }
+
+} /* namespace VevaciousPlusPlus */
