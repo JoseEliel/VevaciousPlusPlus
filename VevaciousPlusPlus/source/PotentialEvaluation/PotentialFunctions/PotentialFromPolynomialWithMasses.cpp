68d0da0a070399ea7966ea9828372b671391d8e6
SimonGeisler <33724461+SimonGeisler@users.noreply.github.com>
2018-06-06 10:06:53 +0200

    Added Parsing of DerivedParameters in Modelfile

--- VevaciousPlusPlus/source/PotentialEvaluation/PotentialFunctions/PotentialFromPolynomialWithMasses.cpp
+++ VevaciousPlusPlus/source/PotentialEvaluation/PotentialFunctions/PotentialFromPolynomialWithMasses.cpp
@@ -57,6 +57,7 @@ namespace VevaciousPlusPlus
     std::string xmlDsbMinimum( "" );
     std::string xmlTreeLevelPotential( "" );
     std::string xmlLoopCorrections( "" );
+    std::string xmlDerivedParameters("");
     xmlParser.OpenRootElementOfFile( modelFilename );
     while( xmlParser.ReadNextElement() )
     {
@@ -67,6 +68,10 @@ namespace VevaciousPlusPlus
       else if( xmlParser.CurrentName() == "DsbMinimum" )
       {
         xmlDsbMinimum = xmlParser.CurrentBody();
+      }
+      else if( xmlParser.CurrentName() == "DerivedParameters" )
+      {
+        xmlDerivedParameters = xmlParser.CurrentBody();
       }
       else if( xmlParser.CurrentName() == "TreeLevelPotential" )
       {
@@ -213,6 +218,39 @@ namespace VevaciousPlusPlus
                                  LHPC::ParsingUtilities::WhitespaceChars() ) );
     }
     // </DsbMinimum>
+  // Now we parse <DerivedParameters> Simon
+    if(!(xmlDerivedParameters.empty())){
+    std::vector<std::pair<std::string,std::string>> derivedParametervector;
+      std::vector< std::string >
+      dPLines( LHPC::ParsingUtilities::SplitBySubstrings( xmlDerivedParameters,
+                                                           "\n" ) );
+      std::string trimmedLine( "" );
+      for( std::vector< std::string >::const_iterator
+           dPLine( dPLines.begin() );
+           dPLine != dPLines.end();
+           ++dPLine )
+      {
+    trimmedLine.assign(*dPLine);
+      trimmedLine.erase(std::remove_if( trimmedLine.begin(),trimmedLine.end(),  ::isspace), trimmedLine.end()); //remove whitespaces
+      if( trimmedLine.empty() )
+        {
+          continue;
+        }
+      size_t equalsPos =  trimmedLine.find( '=' ) ;
+      if( !( equalsPos < ( trimmedLine.size() - 1 ) ) )
+        {
+          std::stringstream errorBuilder;
+          errorBuilder
+          << "Parameter given no value in <DerivedParameters>! (Offending line = \""
+          << *dPLine << "\")";
+          throw std::runtime_error( errorBuilder.str() );
+        }
+      derivedParametervector.push_back(std::make_pair(trimmedLine.substr(0,equalsPos), trimmedLine.substr(equalsPos+1)));
+      }
+    lagrangianParameterManager.RegisterDerivedParameter(derivedParametervector);
+    }
+    // </DerivedParameters>
+      
     // Now we parse <TreeLevelPotential>
     ParseSumOfPolynomialTerms( xmlTreeLevelPotential,
                                treeLevelPotential );
